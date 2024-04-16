eab9e4571e312088ad30dc4c996e5e3d73a9052b
Faiq Mahmood <faiqmahmood95@gmail.com>
2024-04-16 08:23:09 +0200

    added comments

--- problems_1_to_100/javascript/p_01.js
+++ problems_1_to_100/javascript/p_01.js
@@ -1,15 +1,16 @@
 function multiplesOf3Or5(number) {
-  let totalSum = 0;
   /*
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
     Find the sum of all the multiples of 3 or 5 below the provided parameter value number
     */
+  let sumOfMultiples = 0;
+
   for (let num = 1; num < number; num++) {
     if (num % 3 === 0 || num % 5 === 0) {
-      totalSum += num;
+      sumOfMultiples += num;
     }
   }
-  return totalSum;
+  return sumOfMultiples;
 }
 
 console.log(multiplesOf3Or5(9)); // Output: 23
